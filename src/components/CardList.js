d6fc2d1745adf744379adce6fc63968950fe6376
ADOwen <92840021+ADOwen@users.noreply.github.com>
2023-01-16 16:16:17 -0500

    created react project and added card and cardlist component for robots

---
+++ src/components/CardList.js
@@ -0,0 +1,14 @@
+import Card from './Card'
+
+const CardList = ({ robots }) => {
+    return (
+      <div>
+        {robots ? robots.map( robot => {
+          return <Card key={robot.id} {...robot}/>
+        }) : '' }
+      </div>
+      
+    )
+};
+
+export default CardList;
