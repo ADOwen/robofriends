40ce8159c8dd5729e50263ce39cb9fd007ca207b
ADOwen <92840021+ADOwen@users.noreply.github.com>
2023-01-16 16:57:23 -0500

    added some background styling and changed font for page headline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
 // components
 import CardList from './components/CardList'
 import SearchBox from './components/SearchBox';
-
 // robots list
 import {robots} from './robots'
+// styles
+import './App.css'
 
 class App extends React.Component {
   constructor(){
@@ -28,7 +28,7 @@ class App extends React.Component {
 
     return (
       <div className="App tc">
-        <h1>Robofriends</h1>
+        <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchField={this.searchField} searchChange={this.onSearchChange}/>
         <CardList robots={filteredRobots}/>
       </div>
