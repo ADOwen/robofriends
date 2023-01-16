86110a151b50b3f3ebd98b121089e1d40b9a054d
ADOwen <92840021+ADOwen@users.noreply.github.com>
2023-01-16 16:44:14 -0500

    added searchbox component and a ability to filter robots according to search field input

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import React from 'react';
 
 // components
 import CardList from './components/CardList'
+import SearchBox from './components/SearchBox';
 
 // robots list
 import {robots} from './robots'
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      robots: robots,
+      searchField: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchField: event.target.value})
+  }
+
   render () {
+
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    })
+
     return (
-      <div className="App">
-        <CardList robots={robots}/>
+      <div className="App tc">
+        <h1>Robofriends</h1>
+        <SearchBox searchField={this.searchField} searchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots}/>
       </div>
     );
   }
